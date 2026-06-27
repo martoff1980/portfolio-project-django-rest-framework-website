29d2fb24a13c9f14e1081325a79f1a0befcfe874
martoff <martoff@ukr.net>
2026-06-27 10:32:38 +0300

    add middleware and templates

--- railway_station_config/settings.py
+++ railway_station_config/settings.py
@@ -5,7 +5,6 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# ... (SECRET_KEY, DEBUG, ALLOWED_HOSTS оставляем стандартными или берем из os.environ)
 SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # Регистрация приложений
@@ -26,6 +25,31 @@ INSTALLED_APPS = [
     "user",
     "train_station",
     "orders",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
 ]
 
 # Указываем Django использовать нашу кастомную модель пользователя
