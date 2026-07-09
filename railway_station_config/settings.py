e64fb6c6f4fed23cd1f756cd3f90b053894dd75c
martoff <martoff@ukr.net>
2026-07-10 00:18:09 +0300

    modified settings file

--- railway_station_config/settings.py
+++ railway_station_config/settings.py
@@ -97,14 +97,18 @@ else:
 # Settings for Django REST Framework
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ),
     # Default permission classes are set to allow authenticated users
     # to perform any action, while unauthenticated users can only read data.
     # Viewsets that require custom permissions will override this field.
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ),
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
